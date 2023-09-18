cb84626e170f88f0abc80990d77118c39fb4ec62
Vadim Zinchenko <vadim.zinchenko@hereon.de>
2023-09-19 00:55:57 +0200

    Insetr model library template

--- src/models/mdml/mdml_model_library.F90
+++ src/models/mdml/mdml_model_library.F90
@@ -1,2 +1,33 @@
-! TODO: inplement this
+module mdml_model_library
 
+    use fabm_types, only: type_base_model_factory, type_base_model
+ 
+    ! Add use statements for new models here
+ 
+    implicit none
+ 
+    private
+ 
+    type, extends(type_base_model_factory) :: type_factory
+    contains
+       procedure :: create
+    end type
+ 
+    type (type_factory), save, target, public :: mdml_model_factory
+ 
+ contains
+ 
+    subroutine create(self, name, model)
+ 
+       class (type_factory), intent(in) :: self
+       character(*),         intent(in) :: name
+       class (type_base_model), pointer :: model
+ 
+       select case (name)
+          ! Add case statements for new models here
+       end select
+ 
+    end subroutine create
+ 
+ end module
+ 
